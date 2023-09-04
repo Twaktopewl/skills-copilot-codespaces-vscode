c5678943eb6dab165516165c6bc2bded31fc69dd
Twaktopewl <60254781+Twaktopewl@users.noreply.github.com>
2023-09-04 09:33:47 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(val1, val2) {
+  const sum = val1 + val2;
+  const diff = val1 - val2;
+  const mult = val1 * val2;
+  const div = val1 / val2;
+  return [sum, diff, mult, div];
+}
